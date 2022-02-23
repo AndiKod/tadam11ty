d80240741b699e7117470561b5cf3b8ad5bf4505
AndiKod <andrei@andikod.fr>
2022-02-23 04:34:59 +0100

    Added tagList collection

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function (eleventyConfig) {
 
 
   // --- SASS compilation on save ---
- 
+
   // Re compile 11ty on scss changes saved
   eleventyConfig.addWatchTarget("./src/assets/sass/");
   eleventyConfig.addWatchTarget("./src/assets/js/");
@@ -14,14 +14,16 @@ module.exports = function (eleventyConfig) {
   // Public images
   eleventyConfig.addPassthroughCopy('./src/assets/img');
 
+  // --- Collections ---
+  // Create an array of all tags
+  eleventyConfig.addCollection("tagList", require('./lib/collections/tagList'));
+
 
   // --- Shortcodes ---
-
   // {% YouTube {video: "DJztXj2GPfk"} %} Insert a full-width YouTube video
   eleventyConfig.addShortcode('YouTube', require('./lib/shortcodes/youtube'));
 
   // --- Filters ---
-
   // {% for post in collections.posts | limit(-2) %} Returns the 2 most recent posts
   eleventyConfig.addFilter('limit', require('./lib/filters/arr-res-limit'));
 
